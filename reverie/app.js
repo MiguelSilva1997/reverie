987edb52af3a74e654948c636f697421df2f77d3
Miguel Silva <msilva0897@gmail.com>
2018-05-19 02:06:49 -0400

    late commit

--- reverie/app.js
+++ reverie/app.js
@@ -5,10 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var hosts = require('./routes/hosts');
+
 
 var app = express();
+
+const cors = require('cors')
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/hosts', hosts);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
